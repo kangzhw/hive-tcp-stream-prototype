f64d712fe4cee5b4357c325a86858399832d590e
Morgan <winter2718@gmail.com>
2013-02-01 19:24:48 -0600

    Now giving every normalized ip-port combo its own bin during reducing.  Proper flow ranking coming soon.  --About to fly out of the Dallas airport....  :)

--- tcp-reduce.py
+++ tcp-reduce.py
@@ -17,15 +17,34 @@
 import sys
 import atexit
 
-globalFlowList = []
+'''
+Try tracking each flow by combo key with a sliding window for each
+The flow list should keep a running "rank" number for each entry.  This way 
+the results can be sorted according to conversation number and flow rank.
+
+conversation_id:combo1 rank:0
+conversation_id:combo2 rank:0
+conversation_id:combo1 rank:1
+etc....
+'''
+
+globalFlowDict = {}
 
 def exitDump():
-        globalFlowList = sorted(globalFlowList,flowComparator,reverse=True)
-	while globalFlowList != None:
-		print "\t".join(globalFlowList.pop())
+        for key in globalFlowDict.keys():
+		globalFlowDict[key] = sorted(globalFlowDict[key],flowComparator,reverse=True)
+		while len(globalFlowDict[key]) > 1:
+			print '\t'.join(globalFlowDict[key].pop())
 
 #let's not lose track of our last 32 packets.  :)
 atexit.register(exitDump)
+
+def rankAndLabel(flowList):
+	pass
+	'''
+	This should be used to assign a flow_id to each flow.
+	Moves through every flow list using the combo as a key.
+	'''
 
 def flowComparator(x,y):
         if x[2] == y[3]:
@@ -33,39 +52,38 @@ def flowComparator(x,y):
 	else:
 		return 0
 
-def appendFlowToList(line):
+def registerFlow(line):
 	line = line.strip()
         fields = line.split("\t")
         try:
 		#Just keeping things explicit for myself
-                combo = fields[0].split(":")
-                ip1 = combo[0]
-                port1 = combo[1]
-                ip2 = combo[2]
-                port2 = combo[3]
-
-                ts = fields[1]
-                tsmicros = fields[2]
-                tcp_seq = fields[3]
-                tcp_ack = fields[4]
+                combo = fields[0]
+                ts = str(fields[1])
+                tsmicros = str(fields[2])
+                tcp_seq = str(fields[3])
+                tcp_ack = str(fields[4])
                 tcp_flag_syn = fields[5]
                 tcp_flag_ack = fields[6]
+		flow_id = "0"
 		
-		tmpList = [ts,tsmicros,tcp_seq,tcp_ack,tcp_flag_syn,tcp_flag_ack,ip1,port1,ip2,port2]
+		if combo not in globalFlowDict.keys():
+			globalFlowDict[combo] = [] 
+
+		tmpList = [combo,ts,tsmicros,tcp_seq,tcp_ack,tcp_flag_syn,tcp_flag_ack,flow_id]
 
 		if tcp_flag_syn=="true" and tcp_flag_ack=="false":
-			globalFlowList.insert(0,tmpList)
+			globalFlowDict[combo].insert(0,tmpList)
 		elif tcp_flag_syn=="true" and tcp_flag_syn=="true":
-			globalFlowList.insert(1,tmpList)
+			globalFlowDict[combo].insert(1,tmpList)
 		else:
-			globalFlowList.append(tmpList)
-	
+			globalFlowDict[combo].append(tmpList)
         except:
                 pass
 
 for line in sys.stdin:
-	appendFlowToList(line)
-	if len(globalFlowList) > 64:
-		globalFlowList = sorted(globalFlowList,flowComparator,reverse=True)
-		while len(globalFlowList) > 32:
-			print "\t".join(globalFlowList.pop())
+	registerFlow(line)
+	for key in globalFlowDict.keys():
+		if len(globalFlowDict[key]) > 64:
+			globalFlowDict[key] = sorted(globalFlowDict[key],flowComparator,reverse=True)
+			while len(globalFlowDict[key]) > 32:
+				print "\t".join(globalFlowDict[key].pop())
